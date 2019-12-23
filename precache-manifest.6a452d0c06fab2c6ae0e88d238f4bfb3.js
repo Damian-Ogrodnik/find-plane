ef9192c485b24a28d289bb4c807e68fa6afe0736
dogrodnik <d.ogrodnik96@gmail.com>
2019-12-23 09:45:32 +0100

    Updates

---
+++ precache-manifest.6a452d0c06fab2c6ae0e88d238f4bfb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9edfe573edd1bfe03dcbfe2b08470e81",
+    "url": "/find-plane/index.html"
+  },
+  {
+    "revision": "39136a99223a6786c9f9",
+    "url": "/find-plane/static/js/2.d172341d.chunk.js"
+  },
+  {
+    "revision": "9b27b869422f412c9a8a",
+    "url": "/find-plane/static/js/main.9a942f40.chunk.js"
+  },
+  {
+    "revision": "27d988974acd40abb208",
+    "url": "/find-plane/static/js/runtime-main.047bcba6.js"
+  },
+  {
+    "revision": "d24fab1b1e9979952770243834d81cbb",
+    "url": "/find-plane/static/media/airport-icon.d24fab1b.png"
+  },
+  {
+    "revision": "2679cbd1e47875f902bb03768d97a65a",
+    "url": "/find-plane/static/media/location-icon.2679cbd1.png"
+  },
+  {
+    "revision": "16451de917c33f236ced22069c3b448e",
+    "url": "/find-plane/static/media/plane-background.16451de9.jpg"
+  }
+]);
